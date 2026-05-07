778bca0c474a4a9f8fb5d810b5ef1a9a41b72746
protectboss <857042618@qq.com>
2026-05-07 15:20:52 +0800

    1、新建了文件夹用于做moveit实验

---
+++ build/moveit_configs_utils/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sunrise/ws_moveit/install/launch_param_builder/share/launch_param_builder/package.sh"
+. "/home/sunrise/ws_moveit/install/srdfdom/share/srdfdom/package.sh"
